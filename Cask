2605a7b872fb728405ea9efb34de7be6cc3caeac
Gong Qijian <gongqijian@gmail.com>
2021-11-03 20:54:27 +0800

    Init commit
    $ cookiecutter gh:twlz0ne/emacs-package-template

---
+++ Cask
@@ -0,0 +1,6 @@
+(source melpa)
+
+(package "batman-mode" "0.0.1" "Catches criminals.")
+
+(development
+ (depends-on "ert-runner"))
---
+++ test/batman-mode-test.el
@@ -0,0 +1,28 @@
+;;; batman-mode-test.el --- tests for batman-mode.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'ert)
+(require 'batman-mode)
+
+(ert-deftest batman-mode-test/sanity-test ()
+  (should (equal 1 1)))
+
+(provide 'batman-mode-test)
+
+;;; batman-mode-test.el ends here
